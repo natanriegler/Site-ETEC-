a4345de14c0ba8326231a4991b8fe2e0bb2874df
Natan Riegler <natan.silva60@etecia.com.br>
2020-07-23 16:14:14 -0300

    :package: Rodapé

--- script.js
+++ script.js
@@ -3,3 +3,6 @@
 document.querySelector(".hamburguer"). addEventListener("click", () =>
     document.querySelector(".container").classList.toggle("show-menu")
 );
+
+const qtde = document.querySelector("#qtde").nodeValue
+console.log(qtde)
