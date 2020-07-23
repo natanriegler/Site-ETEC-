7387468d82dfa31a18c6cfa3d4833bf8347b6e5c
Natan Riegler <natan.silva60@etecia.com.br>
2020-07-23 17:59:31 -0300

    :package: calculo de orcamento

--- script.js
+++ script.js
@@ -4,5 +4,26 @@ document.querySelector(".hamburguer"). addEventListener("click", () =>
     document.querySelector(".container").classList.toggle("show-menu")
 );
 
-const qtde = document.querySelector("#qtde").nodeValue
-console.log(qtde)
+document.querySelector("#qtde").addEventListener("change", atualizarPreco)
+document.querySelector("#js").addEventListener("change", atualizarPreco)
+document.querySelector("#layout-sim").addEventListener("change", atualizarPreco)
+document.querySelector("#layout-nao").addEventListener("change", atualizarPreco)
+document.querySelector("#prazo").addEventListener("change", function () {
+    const prazo = document.querySelector("#prazo").value
+    document.querySelector("label[for=prazo]").innerHTML = `Prazo: ${prazo} semanas`
+    atualizarPreco()
+})
+function atualizarPreco(){
+    const qtde = document.querySelector("#qtde").value
+    const temJS = document.querySelector("#js").checked
+    const incluiLayout = document.querySelector("#layout-sim").checked
+    const prazo = document.querySelector("#prazo").value
+
+    let preco = qtde * 100;
+    if (temJS) preco *= 1.1
+    if (incluiLayout) preco += 500
+    let taxadeUrgencia = 1 - prazo*0.1;
+    preco *= 1 + taxadeUrgencia
+    
+    document.querySelector("#preco").innerHTML = `R$ ${preco.toFixed(2)}`
+}
